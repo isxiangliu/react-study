6c86a626c10f1dd7766fab200e5e31cd897133cb
liuxiang <xiangliu8116@loongair.cn>
2023-02-15 08:42:30 +0800

    父子通信

--- src/01-base/01-class组件.js
+++ src/01-base/01-class组件.js
@@ -19,10 +19,47 @@
 // console.log(obj.a); 
 
 import React from "react";
-class App extends React.Component{
-    render(){
-        return <div>hello</div>
+
+class Child extends React.Component {
+    state={
+        childMsg:'我是子组件值'
+    }
+    childMethods(){
+        console.log('父组件调用了子组件方法');
+    }
+    render() {
+        const { data } = this.props
+        const { childMsg } = this.state
+        return <div>
+            {data.msg}
+            <button onClick={()=>this.props.getMsg(childMsg)}>pass给父组件</button>
+        </div>
     }
 }
 
+class App extends React.Component {
+    Child=React.createRef()
+    state = {
+        msg: '我是父组件值',
+        changMsg:'我没有改变'
+    }
+    childMsg(val) {
+        console.log(val);
+        this.setState({
+            changMsg:val
+        })
+        // ref访问子组件方法
+        this.Child.current.childMethods()
+        // this.Child.childMethods()
+    }
+    render() {
+        let {changMsg}=this.state
+        return <div>
+            {/* <Child data={this.state} getMsg={this.childMsg.bind(this)}></Child> */}
+            <Child data={this.state} getMsg={(e)=>{this.childMsg(e)}} ref={this.Child}></Child>
+            {/* <Child data={this.state} getMsg={(e)=>{this.childMsg(e)}} ref={node=> this.Child=node}></Child> */}
+            {changMsg}
+        </div>
+    }
+}
 export default App
--- src/01-base/02-函数组件.js
+++ src/01-base/02-函数组件.js
@@ -1,13 +1,45 @@
-function App(){
+import React, { forwardRef, useImperativeHandle, useRef } from "react"
+
+// function Child(props) {
+const Child = forwardRef((props, ref) => {
+    useImperativeHandle(ref, () => {
+        return {
+            childMethods,
+        }
+    })
+    const { msg, passMsg } = props
+    const childMsg = '我是子组件值'
+    function childMethods() {
+        console.log('父组件调用子组件方法');
+    }
     return (
         <div>
-            hello function
+            {msg}
+            <button onClick={() => passMsg(childMsg)}>pass给父组件</button>
+        </div>
+    )
+})
+// }
+
+function App() {
+    // 函数的内部是可以使用 useRef 钩子来获取组件内的 DOM 元素。
+    const ChildRef = useRef()
+    const msg = "我是父组件值"
+    function getSonData(val) {
+        console.log(val);
+        // onRef父组件调用子组件
+        ChildRef.current.childMethods()
+        console.log(ChildRef.current);
+    }
+    return (
+        <div>
+            <Child msg={msg} passMsg={getSonData} ref={ChildRef}></Child>
         </div>
     )
 }
 
 /*
 16.8之前  //无状态组件
-16.8之后  react hooks 
+16.8之后  react hooks
 */
 export default App
